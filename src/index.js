b1624199a5a5fe6a18f1370de7e0d8defaa99dd5
doroshev1ch <doroshevich.2002@mail.ru>
2023-02-09 16:02:00 +0300

    feat: complite task

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  for (let i = 1; i < matrix.length; i+=2){
-    matrix[i] = matrix[i].reverse()
-  } 
-  return matrix.flat()
+  let array = matrix || [];
+  for (let i = 1; i < array.length; i+=2){
+    array[i] = array[i].reverse()
+  }
+  return array.flat()
+  
 }
 
